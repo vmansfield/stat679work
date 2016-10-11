51e53dd6b554ec728bd22dff7f0c7e26255f67e5
Victoria Mansfield <vmansfield@wisc.edu>
2016-10-11 14:33:14 -0500

    submission for homework 1 exercsie 3

--- hw1-snaqTimeTests/readme.md
+++ hw1-snaqTimeTests/readme.md
@@ -8,6 +8,10 @@ This folder is titled 'hw1-snaqTimeTests,' and contains the
 directories 'log' and 'out.' These directories contain the data files used for
 homework exercises.
 
+**Update 10/11/2016:** Edits were made to the 'summarizeSNaQres.sh' file to correct the
+errors from exercise 2. Additionally, edits were made to the file as per the requirements
+for exercise 3.
+
 The following exercises have been completed as part of the first homework
 assignment:
 
@@ -17,7 +21,7 @@ assignment:
    `timetesty_snaq.log` to `timetest0y_snaq.log` where "y" is a digit between 1 and 9.
    Similarly, change `timetesty_snaq.out` to `timetest0y_snaq.out`.
 
-  Note: The 'normalizeFileNames.sh' script to correct the file names for exercise 1
+  **Note:** The 'normalizeFileNames.sh' script to correct the file names for exercise 1
   is located in this folder and corrects the names for the files in the 'log' and 'out'
   folders. The files with the original names (before the zero was added) still remain
   in the folders.
@@ -30,5 +34,25 @@ assignment:
    - "h": the maximum number of hybridizations allowed during the analysis: `hmax`
    - "CPUtime": total CPU time, or "Elapsed time".
 
-   Note: The table output for exercise 2 was saved as 'summarizeSNaQres.csv' and
+   **Note:** The table output for exercise 2 was saved as 'summarizeSNaQres.csv' and
    can be found in this folder along with the script 'summarizeSNaQres.sh.'
+
+   3. Create a script to summarize the results with more information.
+      The script should produce a table in `csv` format with 1 row per analysis,
+      the same columns as before and additional columns for:
+
+      - Nruns: number of runs
+      - Nfail: tuning parameter, "max number of failed proposals"
+      - fabs: tuning parameter called "ftolAbs" in the log file (tolerated
+        difference in the absolute value of the score function, to stop the search)
+      - frel: "ftolRel"
+      - xabs: "xtolAbs"
+      - xrel: "xtolRel"
+      - seed: main seed, i.e. seed for the first runs
+      - under3460: number of runs that returned a network with a score better than
+        (below) 3460
+      - under3450: number of runs with a network score under 3450
+      - under3440: number of runs with a network score under 3440
+
+**Note:** As in exercise 2, the table output for exercise 2 was saved as 'summarizeSNaQres.csv' and
+can be found in this folder along with the script 'summarizeSNaQres.sh.'
--- hw1-snaqTimeTests/summarizeSNaQres.sh
+++ hw1-snaqTimeTests/summarizeSNaQres.sh
@@ -1,23 +1,72 @@
-#script for exercise 2 of homework 1
+#script for exercise 2 and 3 of homework 1
 #set up csv file, can later be read as a table
 
 #create headers of csv
-echo analysis,hmax,elapsedTime > summarizeSNaQres.csv
+echo analysis,hmax,elapsedTime,Nruns,Nfail,fabs,frel,xabs,xrel,seed,under3460,under3450,under3440 > summarizeSNaQres.csv
 
 #for loop to output each line of the csv
 for filename in log/*.log
 do
- 
-  root=$(echo $filename | grep -o '[^\.log]*')
-  #grab the root name of the file
-  #this method will not work for filenames with l o or g, but is useful for completing the exercise
 
-  hmax=$(grep "hmax =" $filename | head -n 1  | grep -o '[[:digit:]]*')
+  root=$(basename -s ".log" $filename)
+  echo $root
+  #grab the root name of the file
+  #using basename command instead of grep, this is more efficient than my previous implementation
+
+  hmax=$(sed -E -n "s/hmax = ([[:digit:]]),/\1/p" log/${root}.log)
   #find the first instance of hmax from the analysis files, grab the first digit
 
   elTime=$(grep "Elapsed time" out/${root}.out | grep -o '[[:digit:]]*\.[[:digit:]]*')
   #find the elapsed time from the file in the out/ folder
 
-  echo $root,$hmax,$elTime >> summarizeSNaQres.csv
+  Nruns=$(grep -o "[[:digit:]]* successful run" out/${root}.out | grep -o "[[:digit:]]*")
+  #find numbr of runs
+
+  Nfail=$(grep -o "max number of failed proposals = [[:digit:]]*" log/${root}.log | grep -o "[[:digit:]]*")
+  #tuning parameter, max number of failed proposals
+
+  fabs=$(grep -o "ftolAbs=[[:digit:]].*[^,]" log/${root}.log | grep -o "[[:digit:]].*")
+  #tuning parameter called "ftolAbs" in the log file (tolerated difference in the absolute value of the score function, to stop the search)
+
+  frel=$(sed -E -n "s/.*ftolRel=([[:digit:]].*), ftolAbs=.*/\1/p" log/${root}.log)
+  #tuning parameter ftolRel
+
+  xabs=$(sed -E -n "s/xtolAbs=([[:digit:]].[[:digit:]]*), xtolRel=.*/\1/p" log/${root}.log )
+  #tuning parameter xtolAbs
+
+  xrel=$(sed -E -n "s/.*xtolRel=([[:digit:]].[[:digit:]]*)./\1/p" log/${root}.log)
+  #tuning parameter xtolRel
+
+  seed=$(sed -E -n "s/main seed ([[:digit:]]*)/\1/p" log/${root}.log)
+  #main seed, seed for the first runs
+
+  loglik=$(sed -E -n "s/.*loglik=([[:digit:]]*).[[:digit:]]* .*/\1/p" log/${root}.log)
+  #loglik variable captures all instances of -loglik in the data files
+  #use if statement or loop to generate under3460, under3450, and under3440 variables
+
+  under3440=0
+  under3450=0
+  under3460=0
+  #initialize variables
+
+  for value in $loglik
+  do
+    if [ $value -lt 3440 ]
+    then
+        ((++under3440))
+    fi
+
+    if [ $value -lt 3450 ]
+    then
+        ((++under3450))
+    fi
+
+    if [ $value -lt 3460 ]
+    then
+        ((++under3460))
+    fi
+  done
+
+  echo $root,$hmax,$elTime,$Nruns,$Nfail,$fabs,$frel,$xabs,$xrel,$seed,$under3460,$under3450,$under3440 >> summarizeSNaQres.csv
   #command to add the comma separated variables to the next line of the csv file
 done
